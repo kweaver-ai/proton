e815bd8cb0e5d741412514905abc5478e78deff4
吴毓喆（Bedivere） <wu.yuzhe@kweaver.ai>
2026-03-17 17:36:03 +0800

    [proton-cli] Add offline-package plane
    Related issue: #1
    
    Signed-off-by: 吴毓喆（Bedivere） <wu.yuzhe@kweaver.ai>

--- proton-cli/cmd/proton-cli/cmd/offline_package/assets.go
+++ proton-cli/cmd/proton-cli/cmd/offline_package/assets.go
@@ -3,6 +3,9 @@ package offline_package
 import _ "embed"
 
 var (
+	//go:embed manifest.yaml
+	manifestBytes []byte
+
 	//go:embed install.sh
 	scriptInstallBytes []byte
 
--- proton-cli/cmd/proton-cli/cmd/offline_package/plan.go
+++ proton-cli/cmd/proton-cli/cmd/offline_package/plan.go
@@ -1,18 +1,13 @@
 package offline_package
 
 import (
-	"log"
-
 	"github.com/spf13/cobra"
 )
 
 func newPlanCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use: "plan [flags]",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			log.Println("Create a plan to building proton-offline-package")
-			return nil
-		},
+		Use: "plan",
+		Run: func(cmd *cobra.Command, _ []string) { cmd.OutOrStdout().Write(manifestBytes) },
 	}
 	return cmd
 }
